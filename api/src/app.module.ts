c4654196cc86ad9c6bbf292a6e1b8b98e77a92ba
Litmus4883 <c202caw@yamaguchi-u.ac.jp>
2024-06-14 16:18:32 +0900

    モジュールを追加

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StringService } from './string/string.service';
+import { StringModule } from './string/string.module';
 
 @Module({
-  imports: [],
+  imports: [StringModule],
   controllers: [AppController],
   providers: [AppService, StringService],
 })
