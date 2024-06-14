393a97384ebef9bd47d623b645dd7a7f94801d0d
Litmus4883 <c202caw@yamaguchi-u.ac.jp>
2024-06-14 17:23:00 +0900

    dtoの作成

--- api/src/string/string.controller.ts
+++ api/src/string/string.controller.ts
@@ -1,17 +1,18 @@
 import { Body, Controller, Get } from '@nestjs/common';
 import { StringService } from './string.service';
+import { StringDto } from './string.dto';
 
 @Controller('string')
 export class StringController {
   constructor(private stringService: StringService) {}
 
   @Get('uppercase')
-  getUpperCase(@Body() body: { message: string }): string {
-    return this.stringService.upperCase(body.message);
+  getUpperCase(@Body() stringDto: StringDto): string {
+    return this.stringService.upperCase(stringDto.message);
   }
 
   @Get('lowercase')
-  getLowerCase(@Body() body: { message: string }): string {
-    return this.stringService.lowerCase(body.message);
+  getLowerCase(@Body() stringDto: StringDto): string {
+    return this.stringService.lowerCase(stringDto.message);
   }
 }
