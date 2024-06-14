7cc96797e99d2adb03a9162dd24f3a9e703e1b2b
Litmus4883 <c202caw@yamaguchi-u.ac.jp>
2024-06-14 11:11:18 +0900

    大小文字変換を実装

--- api/src/string/string.service.spec.ts
+++ api/src/string/string.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StringService } from './string.service';
+import { resourceLimits } from 'worker_threads';
 
 describe('StringService', () => {
   let service: StringService;
@@ -12,7 +13,17 @@ describe('StringService', () => {
     service = module.get<StringService>(StringService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+  describe('文字列操作のテスト', () => {
+    it('messageの"hogehoge"を受けとったとき、すべて大文字に変換していること。', () => {
+      const message: string = "hogehoge";
+      const upperString = service.upperCase(message);
+      expect(upperString).toBe("HOGEHOGE");
+    });
+
+    it('messageをすべて小文字に変換していること。', () => {
+      const message: string = "HOGEHOGE";
+      const lowerString = service.lowerCase(message);
+      expect(lowerString).toBe("hogehoge");
+    });
+  })
 });
--- api/src/string/string.service.ts
+++ api/src/string/string.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class StringService {}
+export class StringService {
+  // TODO: messageを受け取る。
+  readonly upperCase = (message: string): string => {
+    // TODO: uppercaseに変換。
+    return message.toUpperCase();
+  }
+
+  readonly lowerCase = (message: string): string => {
+    return message.toLowerCase();
+  }
+}
