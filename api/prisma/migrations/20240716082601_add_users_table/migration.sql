3b0484917e9b1642ab2928871815f54c2604f27b
TaigaTomonaga <taigatomonaga@gmail.com>
2024-07-16 17:47:23 +0900

    usersテーブルの作成

--- api/prisma/migrations/20240716082601_add_users_table/migration.sql
+++ api/prisma/migrations/20240716082601_add_users_table/migration.sql
@@ -2,7 +2,10 @@
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
+    "hashedPassword" TEXT NOT NULL,
     "name" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
