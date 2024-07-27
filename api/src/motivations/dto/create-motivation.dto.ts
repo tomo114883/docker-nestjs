6da9610cb56cff26e61516544f099023d4d2696f
TaigaTomonaga <taigatomonaga@gmail.com>
2024-07-27 18:03:17 +0900

    serviceの基本の実装

--- api/src/motivations/dto/create-motivation.dto.ts
+++ api/src/motivations/dto/create-motivation.dto.ts
@@ -1,1 +1,15 @@
-export class CreateMotivationDto {}
+import { createZodDto } from 'nestjs-zod';
+import { z } from 'zod';
+
+export const CreateMotivationSchema = z.object({
+  id: z.number().optional(), // 自動生成されるのでオプショナル
+  name: z.string(),
+  weight: z.number().nullable().optional(),
+  userId: z.number(),
+  typeId: z.number().nullable().optional(),
+  createdAt: z.string().datetime({ offset: true }).optional(), // 自動生成されるのでオプショナル
+  updatedAt: z.string().datetime({ offset: true }).optional(), // 自動生成されるのでオプショナル
+  deletedAt: z.string().datetime({ offset: true }).nullable().optional(),
+});
+
+export class CreateMotivationDto extends createZodDto(CreateMotivationSchema) {}
--- api/src/motivations/motivations.service.ts
+++ api/src/motivations/motivations.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMotivationDto } from './dto/create-motivation.dto';
 import { UpdateMotivationDto } from './dto/update-motivation.dto';
+import { Motivation } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class MotivationsService {
-  create(createMotivationDto: CreateMotivationDto) {
-    return 'This action adds a new motivation';
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: CreateMotivationDto): Promise<Motivation> {
+    return await this.prisma.motivation.create({
+      data: {
+        name: data.name,
+        weight: data.weight,
+        userId: data.userId,
+        typeId: data.typeId,
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all motivations`;
+  async findAll(): Promise<Motivation[]> {
+    return await this.prisma.motivation.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} motivation`;
+  async findOne(id: number): Promise<Motivation> {
+    return await this.prisma.motivation.findUnique({ where: { id } });
   }
 
-  update(id: number, updateMotivationDto: UpdateMotivationDto) {
-    return `This action updates a #${id} motivation`;
+  async update(id: number, data: UpdateMotivationDto) {
+    return await this.prisma.motivation.update({ where: { id }, data });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} motivation`;
+  async remove(id: number) {
+    const deletedMotiv = await this.prisma.motivation.delete({ where: { id } });
+    return deletedMotiv.id;
   }
 }
