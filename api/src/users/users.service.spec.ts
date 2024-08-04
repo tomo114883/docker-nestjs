be9b49dc53c0e07566d3afe1d8cc3eb8ce96c935
TaigaTomonaga <taigatomonaga@gmail.com>
2024-08-04 15:02:20 +0900

    resolve error of users

--- api/src/users/users.service.spec.ts
+++ api/src/users/users.service.spec.ts
@@ -1,13 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       providers: [UsersService],
-    }).compile();
+    })
+      // Add to be auto-transaction.
+      .overrideProvider(PrismaService)
+      .useValue(jestPrisma.client)
+      .compile();
 
     service = module.get<UsersService>(UsersService);
   });
