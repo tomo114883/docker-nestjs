37276ba5a8a5204fd870eaf9e948ca131d855b83
TaigaTomonaga <taigatomonaga@gmail.com>
2024-08-09 19:19:57 +0900

    Dont return user profile

---
+++ api/src/auth/constants.example.ts
@@ -0,0 +1,6 @@
+// Setting a secret key of JWT.
+
+export const jwtConstants = {
+  secret:
+    'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
+};
