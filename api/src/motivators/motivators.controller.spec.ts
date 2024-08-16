fcfd8388bdacd8155185bdbcf6e21843cf750708
TaigaTomonaga <taigatomonaga@gmail.com>
2024-08-16 13:52:37 +0900

    modify the motiv dir lately.

--- api/src/motivators/motivators.controller.spec.ts
+++ api/src/motivators/motivators.controller.spec.ts
@@ -10,11 +10,11 @@ import {
 } from 'src/test.utils/factory';
 
 describe('MotivatorsController', () => {
-  let controller: MotivatorsController;
+  let motivatorsController: MotivatorsController;
   let motivatorsService: DeepMocked<MotivatorsService>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const motivatorsModule: TestingModule = await Test.createTestingModule({
       controllers: [MotivatorsController],
       providers: [
         {
@@ -24,9 +24,10 @@ describe('MotivatorsController', () => {
       ],
     }).compile();
 
-    controller = module.get<MotivatorsController>(MotivatorsController);
+    motivatorsController =
+      motivatorsModule.get<MotivatorsController>(MotivatorsController);
     motivatorsService =
-      module.get<DeepMocked<MotivatorsService>>(MotivatorsService);
+      motivatorsModule.get<DeepMocked<MotivatorsService>>(MotivatorsService);
   });
 
   describe('create-method', () => {
@@ -47,7 +48,7 @@ describe('MotivatorsController', () => {
 
       // Trigger this Controller method to test its functionality.
       // By calling this method, the mocked Service can be available.
-      await controller.create(input);
+      await motivatorsController.create(input);
 
       // Verify that the appropriate Service mothod is called and given the argument.
       expect(motivatorsService.create).toHaveBeenCalledWith(input);
@@ -56,7 +57,7 @@ describe('MotivatorsController', () => {
 
   describe('findAll-method', () => {
     it('Call the appropriate method and use the input data.', async () => {
-      await controller.findAll();
+      await motivatorsController.findAll();
 
       expect(motivatorsService.findAll).toHaveBeenCalledWith();
     });
@@ -66,31 +67,25 @@ describe('MotivatorsController', () => {
     it('Call the appropriate method and use the input data.', async () => {
       const motivator = await MotivatorModelFactory.create();
 
-      // Same above.
-      await controller.findOne(motivator.id);
+      await motivatorsController.findOne(motivator.id);
 
-      // Same above.
       expect(motivatorsService.findOne).toHaveBeenCalledWith(motivator.id);
     });
   });
 
   describe('update-method', () => {
     it('Call the appropriate method and use the input data.', async () => {
-      // Same above.
       const motivator = await MotivatorModelFactory.create();
       const type = await TypeModelFactory.create();
 
-      // Same above.
       const input = {
         name: faker.word.noun(),
         weight: faker.number.int({ min: 1, max: 5 }),
         typeId: type.id,
       };
 
-      // Same above.
-      await controller.update(motivator.id, input);
+      await motivatorsController.update(motivator.id, input);
 
-      // Same above.
       expect(motivatorsService.update).toHaveBeenCalledWith(
         motivator.id,
         input,
@@ -100,13 +95,10 @@ describe('MotivatorsController', () => {
 
   describe('remove-method', () => {
     it('Call the appropriate method and use the input data.', async () => {
-      // Same above.
       const motivator = await MotivatorModelFactory.create();
 
-      // Same above.
-      await controller.remove(motivator.id);
+      await motivatorsController.remove(motivator.id);
 
-      // Same above.
       expect(motivatorsService.remove).toHaveBeenCalledWith(motivator.id);
     });
   });
--- api/src/motivators/motivators.module.ts
+++ api/src/motivators/motivators.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MotivatorsService } from './motivators.service';
 import { MotivatorsController } from './motivators.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [MotivatorsController],
-  providers: [MotivatorsService],
+  providers: [MotivatorsService, PrismaService],
 })
 export class MotivatorsModule {}
