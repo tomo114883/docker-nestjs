ef0b7a86bcb876bc2d3cc52d57f93fe156a591af
TaigaTomonaga <taigatomonaga@gmail.com>
2025-01-18 15:21:03 +0900

    test: add some tests for factors-sets

--- api/src/factors-sets/factors-sets.controller.spec.ts
+++ api/src/factors-sets/factors-sets.controller.spec.ts
@@ -1,20 +1,46 @@
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Request } from 'express';
+import { CreateFactorsSetDto } from './dto/create-factors-set.dto';
 import { FactorsSetsController } from './factors-sets.controller';
 import { FactorsSetsService } from './factors-sets.service';
 
 describe('FactorsSetsController', () => {
-  let controller: FactorsSetsController;
+  let factorsSetsController: FactorsSetsController;
+  let factorsSetsService: DeepMocked<FactorsSetsService>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const factorsSetsModule: TestingModule = await Test.createTestingModule({
       controllers: [FactorsSetsController],
-      providers: [FactorsSetsService],
+      providers: [
+        {
+          provide: FactorsSetsService,
+          useValue: createMock<FactorsSetsService>(),
+        },
+      ],
     }).compile();
 
-    controller = module.get<FactorsSetsController>(FactorsSetsController);
+    factorsSetsController = factorsSetsModule.get<FactorsSetsController>(
+      FactorsSetsController,
+    );
+    factorsSetsService =
+      factorsSetsModule.get<DeepMocked<FactorsSetsService>>(FactorsSetsService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('create', () => {
+    it('should call the appropriate method and use the input data.', async () => {
+      const req = { user: { id: 1 } } as Request;
+      const dto: CreateFactorsSetDto = { name: 'Test Factors Set' };
+      await factorsSetsController.create(req, dto);
+      expect(factorsSetsService.create).toHaveBeenCalledWith(req.user.id, dto);
+    });
+  });
+
+  describe('findAllNames', () => {
+    it('should call the appropriate method and use the input data.', async () => {
+      const req = { user: { id: 1 } } as Request;
+      await factorsSetsController.findAllNames(req);
+      expect(factorsSetsService.findAllNames).toHaveBeenCalledWith(req.user.id);
+    });
   });
 });
--- api/src/factors-sets/factors-sets.service.spec.ts
+++ api/src/factors-sets/factors-sets.service.spec.ts
@@ -1,18 +1,63 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UserModelFactory } from 'src/test.utils/factory';
+import { CreateFactorsSetDto } from './dto/create-factors-set.dto';
 import { FactorsSetsService } from './factors-sets.service';
 
 describe('FactorsSetsService', () => {
-  let service: FactorsSetsService;
+  let factorsSetsService: FactorsSetsService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       providers: [FactorsSetsService],
-    }).compile();
+    })
+      .overrideProvider(PrismaService)
+      .useValue(jestPrisma.client)
+      .compile();
 
-    service = module.get<FactorsSetsService>(FactorsSetsService);
+    factorsSetsService = module.get<FactorsSetsService>(FactorsSetsService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('create', () => {
+    it('should create a new factors set in the DB when the data is entered.', async () => {
+      const user = await UserModelFactory.create();
+
+      const dto: CreateFactorsSetDto = { name: 'Test Factors Set' };
+
+      const factorsSet = await factorsSetsService.create(user.id, dto);
+
+      const result = await prismaService.factorsSet.findUnique({
+        where: { id: factorsSet.id },
+      });
+
+      expect(factorsSet.id).not.toBeNull();
+      expect(factorsSet.name).toBe(dto.name);
+      expect(factorsSet.userId).toBe(user.id);
+      expect(result.id).toBe(factorsSet.id);
+      expect(result.name).toBe(factorsSet.name);
+      expect(result.userId).toBe(factorsSet.userId);
+    });
+  });
+
+  describe('findAllNames', () => {
+    it('should get all factors sets names for a user.', async () => {
+      const user = await UserModelFactory.create();
+
+      const factorsSet = await prismaService.factorsSet.create({
+        data: {
+          name: 'Test Factors Set',
+          userId: user.id,
+        },
+      });
+
+      const gotFactorsSets = await factorsSetsService.findAllNames(user.id);
+
+      expect(gotFactorsSets[0].id).toBe(factorsSet.id);
+      expect(gotFactorsSets[0].name).toBe(factorsSet.name);
+    });
   });
 });
