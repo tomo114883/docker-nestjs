9090a203d9d32f455ad20587e94e1520d3c94533
TaigaTomonaga <taigatomonaga@gmail.com>
2025-02-01 09:42:24 +0900

    chore: correct tests for the auth service and controller

--- api/src/auth/auth.service.spec.ts
+++ api/src/auth/auth.service.spec.ts
@@ -1,10 +1,13 @@
-import * as bcrypt from 'bcrypt';
-import { PrismaModule } from 'src/prisma/prisma.module';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { faker } from '@faker-js/faker';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
+import { User } from '@prisma/client';
+import * as bcrypt from 'bcryptjs';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UserModelFactory } from 'src/test.utils/factory';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 import { JwtStrategy } from './jwt.strategy';
@@ -14,8 +17,8 @@ describe('AuthService', () => {
   let authService: AuthService;
   let prismaService: PrismaService;
 
-  // Code here runs before each test cases in this suite.
-  // and this function, beforeEach(), is used in testing like Jest.
+  // This codes run before each test cases run.
+  // and this function, beforeEach(), is used in tests like Jest.
   beforeEach(async () => {
     const authModule: TestingModule = await Test.createTestingModule({
       // Dependencies from @Module in the AuthModule to set env.
@@ -25,27 +28,23 @@ describe('AuthService', () => {
           secret: 'secret-for-test',
           signOptions: { expiresIn: '180s' },
         }),
-        // Import the PassportModule.
         PassportModule,
-        // Imports the PrismaModule has access to the PrismaService.
         PrismaModule,
       ],
-      // Setting the LocalStrategy and the JwtStrategy as the providers.
       providers: [AuthService, LocalStrategy, JwtStrategy],
     })
-      // Add to be auto-transaction.
+      // Add following two lines for the auto-transaction.
       .overrideProvider(PrismaService)
       .useValue(jestPrisma.client)
       .compile();
 
-    // This "module" was declared above.
     authService = authModule.get<AuthService>(AuthService);
     prismaService = authModule.get<PrismaService>(PrismaService);
   });
 
   describe('validateUser', () => {
-    it('verify if the password is hashed.', async () => {
-      // Create a plane password to compare with the hashed password..
+    it('should verify if the password is hashed.', async () => {
+      // Create a plane password to compare with a hashed password..
       const planePassword = faker.internet.password();
 
       // Hash the password with salt to input to the DB.
@@ -60,44 +59,71 @@ describe('AuthService', () => {
         },
       });
 
-      // Create a input data to verify the validateUser-method.
-      const input = {
-        email: user.email,
-        password: planePassword,
-      };
-
-      const result = await authService.validateUser(input);
+      // Compare the plane password with the hashed password.
+      const result = await authService.validateUser(user.email, planePassword);
 
       expect(result.email).toBe(user.email);
       expect(result.name).toBe(user.name);
       expect(result).not.toHaveProperty('password');
     });
 
-    it('Return null when the user does not exist.', async () => {
-      // Create a input data to verify the validateUser-method.
-      const input = {
-        email: faker.internet.exampleEmail(),
-        password: faker.internet.password(),
-      };
+    it('should return null when the user does not exist.', async () => {
+      const email = faker.internet.exampleEmail();
+      const password = faker.internet.password();
 
-      const result = await authService.validateUser(input);
+      const result = await authService.validateUser(email, password);
 
       expect(result).toBeNull();
     });
   });
 
-  describe('signIn', () => {
-    it('return the access token when the data was input.', async () => {
-      const user: AuthDto = {
-        id: faker.number.int(),
-        name: faker.person.firstName(),
+  describe('signUp', () => {
+    it('should create a new user.', async () => {
+      const dto: AuthDto = {
+        email: faker.internet.exampleEmail(),
+        password: faker.internet.password(),
       };
 
-      // Return a access token to signIn.
-      const result = await authService.signIn(user);
+      const result: User = await authService.signUp(dto);
 
-      // Verify if returned a access token.
-      expect(result).toHaveProperty('access_token');
+      expect(result.email).toBe(dto.email);
+      expect(result.password).not.toBe(dto.password);
+      expect(result.name).toBeNull();
+    });
+  });
+
+  describe('logIn', () => {
+    it('should return a access token when a data is input.', async () => {
+      const dto: AuthDto = {
+        email: faker.internet.exampleEmail(),
+        password: faker.internet.password(),
+      };
+
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(dto.password, salt);
+
+      await prismaService.user.create({
+        data: {
+          email: dto.email,
+          password: hashedPassword,
+        },
+      });
+
+      // Return a access token to signIn.
+      const result = await authService.login(dto);
+
+      expect(result).toHaveProperty('accessToken');
+    });
+  });
+
+  describe('generateJwt', () => {
+    it('should generate an access_token.', async () => {
+      const user: User = await UserModelFactory.create();
+
+      // Return a access token to signIn.
+      const result = await authService.generateJwt(user.id, user.email);
+
+      expect(result).toHaveProperty('accessToken');
     });
   });
 });
